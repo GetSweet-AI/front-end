e88540cbf5d4736741b50ad22d9348a27b079086
nathantross <nathantross@gmail.com>
2023-07-27 22:45:20 -0700

    Updated layout of homepage slightly

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -213,7 +213,7 @@ export default function Home() {
 
       <main className="isolate">
         {/* Hero section */}
-        <div className="relative pt-14">
+        <div className="relative pt-12">
           <div
             className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
             aria-hidden="true"
@@ -226,16 +226,16 @@ export default function Home() {
               }}
             />
           </div>
-          <div className="py-24 sm:py-16">
+          <div className="py-8 sm:py-16">
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
               <div className="mx-auto max-w-2xl text-center">
                 <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
                   100% Automate your <span className="text-pink-600">social media</span> with AI
                 </h1>
-                <p className="mt-6 text-lg leading-8 text-gray-600">
+                <p className="mt-4 text-md leading-8 text-gray-600">
                   Replace ChatGPT and other tools you use for social media with GetSweet.AI to 100% automate your content creation and posting.
                 </p>
-                <div className="mt-10 flex items-center justify-center gap-x-6">
+                <div className="mt-6 flex items-center justify-center gap-x-6">
                   <a
                     href="/signup"
                     className="rounded-md bg-indigo-600 px-12 py-2.5 text-lg font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
