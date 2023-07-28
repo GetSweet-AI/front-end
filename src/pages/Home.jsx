fb56dae8fc09f9eee598fdee88d7932e80a836f6
nathantross <nathantross@gmail.com>
2023-07-27 19:32:23 -0700

    Updated get started button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -174,7 +174,7 @@ export default function Home() {
                 <span className="sr-only">Your Company</span>
                 <img
                   className="h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                  src= {logo}
                   alt=""
                 />
               </a>
@@ -240,7 +240,7 @@ export default function Home() {
                 </p>
                 <div className="mt-10 flex items-center justify-center gap-x-6">
                   <a
-                    href="#"
+                    href="/signup"
                     className="rounded-md bg-indigo-600 px-12 py-2.5 text-lg font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                   >
                     Get started for free
