2f83388226165f042ddc5dc65b39a07fa0ff3be5
nathantross <nathantross@gmail.com>
2023-07-31 21:37:26 -0700

    Added Google Analytics - base tracking without customization

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
+import ReactGA from "react-ga4";
+
+// Initialize GA4
+ReactGA.initialize('G-RXSQ0HH7M8');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -14,3 +18,14 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </Router>
   </React.StrictMode>
 );
+// GA4 Code
+const SendAnalytics = ()=> {
+  ReactGA.send({
+    hitType: "pageview",
+    page: window.location.pathname,
+  });
+}
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals(SendAnalytics);
