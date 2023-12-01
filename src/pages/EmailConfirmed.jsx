ee880891a2d47dc071d93edb0362c39d941e8e21
Hicham Zaidi <hzaidi@unibitsoft.com>
2023-12-01 22:50:57 +0100

    new featuers

--- src/pages/EmailConfirmed.jsx
+++ src/pages/EmailConfirmed.jsx
@@ -15,9 +15,9 @@ export default function EmailConfirmed() {
 
 
     useEffect(() => {
-        const confirmationLink = `http://localhost:5000/api/v1/auth/users/confirm-email/${userId}`;
+        const confirmationLink = `https://seashell-app-2-n2die.ondigitalocean.app/api/v1/auth/users/confirm-email/${userId}`;
 
-        axios.get(`http://localhost:5000/api/v1/auth/users/${userId}`).then((res) => {
+        axios.get(`https://seashell-app-2-n2die.ondigitalocean.app/api/v1/auth/users/${userId}`).then((res) => {
             axios.put(confirmationLink)
                 .then(response => {
                     // Handle the response if needed
